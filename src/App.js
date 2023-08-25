5702c965667cfe0077d7d35e8db0c7ccebe3cac6
Azheem Rahman <azheem.rahman@gmail.com>
2023-08-25 22:18:30 +0800

    added Employee component to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import employeeData from "./employeeData";
+import Employee from "./Employee";
 
 export default class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <>
+        Hello World!
+        <Employee employeeData={employeeData} />
+      </>
+    );
   }
 }
