81129c8873bba3cfd0e85cae2718834461659e23
Azheem Rahman <azheem.rahman@gmail.com>
2023-08-25 22:44:40 +0800

    added grocery data array

---
+++ src/groceryData.js
@@ -0,0 +1,31 @@
+const groceryData = [
+  { id: 1, name: "Apples", category: "Fruits", price: 1.99, quantity: 3 },
+  { id: 2, name: "Milk", category: "Dairy", price: 2.49, quantity: 1 },
+  { id: 3, name: "Bread", category: "Bakery", price: 1.89, quantity: 2 },
+  { id: 4, name: "Chicken", category: "Meat", price: 5.99, quantity: 2 },
+  { id: 5, name: "Carrots", category: "Vegetables", price: 0.99, quantity: 1 },
+  { id: 6, name: "Bananas", category: "Fruits", price: 0.69, quantity: 6 },
+  { id: 7, name: "Eggs", category: "Dairy", price: 3.29, quantity: 1 },
+  { id: 8, name: "Bagels", category: "Bakery", price: 2.99, quantity: 4 },
+  { id: 9, name: "Beef", category: "Meat", price: 8.49, quantity: 1 },
+  { id: 10, name: "Lettuce", category: "Vegetables", price: 1.29, quantity: 1 },
+  { id: 11, name: "Oranges", category: "Fruits", price: 1.49, quantity: 4 },
+  { id: 12, name: "Yogurt", category: "Dairy", price: 1.79, quantity: 3 },
+  { id: 13, name: "Cookies", category: "Snacks", price: 2.99, quantity: 1 },
+  { id: 14, name: "Pasta", category: "Pantry", price: 1.49, quantity: 2 },
+  { id: 15, name: "Salmon", category: "Seafood", price: 10.99, quantity: 1 },
+  {
+    id: 16,
+    name: "Tomatoes",
+    category: "Vegetables",
+    price: 0.79,
+    quantity: 5,
+  },
+  { id: 17, name: "Grapes", category: "Fruits", price: 2.99, quantity: 2 },
+  { id: 18, name: "Cheese", category: "Dairy", price: 4.29, quantity: 1 },
+  { id: 19, name: "Chips", category: "Snacks", price: 1.99, quantity: 3 },
+  { id: 20, name: "Rice", category: "Pantry", price: 3.49, quantity: 1 },
+  { id: 21, name: "Shrimp", category: "Seafood", price: 12.99, quantity: 1 },
+];
+
+export default groceryData;
